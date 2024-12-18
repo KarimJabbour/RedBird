9b31645c04466451b580714abc1690bf14c03d2e
Karim Jabbour <kareemjabbour@gmail.com>
2024-12-17 20:15:48 -0500

    Route index.php

--- index.php
+++ index.php
@@ -1,10 +1,4 @@
 <?php
-include 'includes/header.php';
-?>
-
-<h1>Welcome to the SOCS Booking Tool!</h1>
-<p><a href="pages/login.php">Login</a> or <a href="pages/dashboard.php">Dashboard</a></p>
-
-<?php
-include 'includes/footer.php';
+header("Location: pages/landing.html");
+exit();
 ?>
